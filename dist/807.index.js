78d6199b2b63d4ee58f327ac8012d845cd24ccbf
Kyle Johnson <kylejohnson.mn@gmail.com>
2024-06-03 18:27:36 -0500

    fix: include all of dist/* from 'npm run all' (#10)
    Signed-off-by: Kyle Johnson <kylejohnson.mn@gmail.com>

---
+++ dist/807.index.js
@@ -0,0 +1,15 @@
+export const id = 807;
+export const ids = [807];
+export const modules = {
+
+/***/ 24807:
+/***/ ((module) => {
+
+module.exports = eval("require")("smee-client");
+
+
+/***/ })
+
+};
+
+//# sourceMappingURL=807.index.js.map
