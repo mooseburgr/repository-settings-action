54ba1cce61931f5afb9a01d8b930401cc217a9ad
Kyle Johnson <kylejohnson.mn@gmail.com>
2024-06-26 15:26:04 -0500

    exit 1 if run errors

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { run } from '@probot/adapter-github-actions'
 import app from '@repository-settings/app'
 
-run(app)
+run(app).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
